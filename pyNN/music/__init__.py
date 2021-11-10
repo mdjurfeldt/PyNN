568c884c8edb7cffb56a729d1922770ece961333
Mikael Djurfeldt <mikael@djurfeldt.com>
2021-11-10 04:17:07 +0100

    Use py3 module paths

--- pyNN/music/__init__.py
+++ pyNN/music/__init__.py
@@ -1,1 +1,1 @@
-from multisim import Config, local_backend, setup, run, Projection, Port, connectPorts, end, is_proxy
+from pyNN.music.multisim import Config, local_backend, setup, run, Projection, Port, connectPorts, end, is_proxy
